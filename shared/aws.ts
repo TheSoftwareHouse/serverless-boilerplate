a48f700eb38dd5dde936c0e5add9b42039e09e56
Adam Polak <adampolak@Adams-MacBook-Pro.local>
2020-10-25 14:54:57 +0100

    Updated boilerplate

--- shared/aws.ts
+++ shared/aws.ts
@@ -1,9 +1,9 @@
 import { commonHeaders } from "./headers";
 
-export const awsLambdaResponse = (statusCode: number, body: any) => {
+export const awsLambdaResponse = (statusCode: number, body?: any) => {
   return {
     statusCode,
-    body: JSON.stringify(body),
+    body: body ? JSON.stringify(body) : body,
     headers: {
       ...commonHeaders,
     },
