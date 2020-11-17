61e8605795eeb4b0eb4011cfca3f0b718d8dba99
Marcin Kopa <marcin.info@gmail.com>
2020-11-17 11:33:51 +0100

    Update supertest, fix handler.spec.ts

--- lambdas/migrations/handler.ts
+++ lambdas/migrations/handler.ts
@@ -23,8 +23,7 @@ export async function handle(__: any, _: Context): Promise<any> {
       success: true,
     });
   } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e);
+    winstonLogger.error(e.message);
 
     return handleError(e);
   }
