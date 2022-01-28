722a86add027faa803b558c708eb9b53e5e18f20
Adam Polak <adampolak@Adams-MacBook-Pro.local>
2022-01-28 08:13:15 +0100

    fix: migrations lambda shouldn't return http response

--- functions/migrations/handler.ts
+++ functions/migrations/handler.ts
@@ -1,11 +1,8 @@
 import { Context } from "aws-lambda";
 import middy from "@middy/core";
 
-import { awsLambdaResponse } from "../../shared/aws";
 import { winstonLogger } from "../../shared/logger";
 import { ConnectionManager } from "../../shared/utils/connection-manager";
-import { inputOutputLoggerConfigured } from "../../shared/middleware/input-output-logger-configured";
-import { customHttpErrorHandler } from "../../shared/middleware/custom-http-error-handler";
 
 export const handle = middy(async function handle(__: any, _: Context): Promise<any> {
   winstonLogger.info("Pre connection");
@@ -17,9 +14,5 @@ export const handle = middy(async function handle(__: any, _: Context): Promise<
 
   winstonLogger.info("Post connection");
 
-  return awsLambdaResponse(200, {
-    success: true,
-  });
-})
-  .use(inputOutputLoggerConfigured())
-  .use(customHttpErrorHandler());
+  return;
+});
